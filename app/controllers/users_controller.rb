44749e2dfa60b5f403d57e91a4252b38cfc3d1fc
Kim Nguyen <nnhk23@gmail.com>
2020-12-07 16:43:06 -0800

    minor changes in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
     def index
         users = User.all
-        render json: users, only: [:id, :name, :zip_code], include: {:appointments => {only: [:start_date, :end_date, :pet_id]}}
+        render json: users, only: [:id, :name, :zip_code], include: {:appointments => {only: [:id, :start_date, :end_date, :pet_id]}}
     end
 
     def show
